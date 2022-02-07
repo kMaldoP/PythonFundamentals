1eacee9e23dc1a0f5cea9d9fe13d850ea72b233a
krysmaldonado <krysm2015@gmail.com>
2022-02-07 13:11:48 -0800

    first week

---
+++ cc1.py
@@ -0,0 +1,16 @@
+
+# Turn this line into a comment
+
+cities = ["Seattle", "Tacoma", "Bellevue"]
+
+a = 1
+print(type(a))
+
+b = float(12.35)
+print(type(b))
+
+c = "testing"
+print(type(c))
+
+d = a + b
+print(d)
